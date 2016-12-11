3892743ebd30a7d971acc419c947ff109d3b9357
Michael Morisi <michaelmorisi@Michaels-MacBook-Pro.local>
2016-12-11 14:26:42 -0500

    Added Affectiva

--- tools.js
+++ tools.js
@@ -1,11 +1,8 @@
 
-/*
- * Global list of tools, indexed by (user-visible) tool name
- */
 tools = {};
 
 /*
- * Individual controls/widgets
+ * Buttons
  */
 
 class ToolControl { 
@@ -27,7 +24,6 @@ class ToolControl {
 
 class ColorControl {
     constructor (cx) {
-    // Some browsers will provide a color picker, others default to text field
     var input = elt("input", {type: "color"});
 
     input.addEventListener("change", function() {
@@ -70,11 +66,8 @@ class EraseButton {
 
 }
 
-/*
- * Drawing modes (tools)
- */
+/* Pointer tools */
 
-/* Base class contains common routines, used by several subclasses */
 class Tool {
     trackDrag(onMove, onEnd) { 
     addEventListener("mousemove", onMove); 
@@ -87,10 +80,6 @@ class Tool {
     }
     }
 
-    /*
-     * Utility function: Convert from window-relative to element-relative position
-     * event.clientX and element.getBoundingClientRect() are both relative to top-left of screen
-     */
     static relativePos(event, element) {
     var rect = element.getBoundingClientRect();
     return {x: Math.floor(getX()),
@@ -99,26 +88,17 @@ class Tool {
 }
 
 class LineTool extends Tool { 
-    down (event, cx, onEnd) {    
-    cx.lineCap = "round";
+        down (event, cx, onEnd) {    
+        cx.lineCap = "round";
 
-    var pos = Tool.relativePos (event, cx.canvas);
-    this.trackDrag(function(event) { 
-        cx.beginPath(); 
-        cx.moveTo(pos.x, pos.y); 
-        pos = Tool.relativePos(event, cx.canvas); 
-        cx.lineTo(pos.x, pos.y); 
-        cx.stroke(); 
-    }, onEnd); 
-   /* cx.lineCap = "round";
-    var pos = Tool.relativePos(event, cx.canvas);
-    this.trackDrag(function(event) {
-        cx.beginPath();
-        cx.arc(faceX, faceY, 1, 0, 2*Math.PI);
-        cx.fillStyle = 'black';
-        cx.fill();
-        cx.closePath();
-    });*/
+        var pos = Tool.relativePos (event, cx.canvas);
+        this.trackDrag(function(event) { 
+            cx.beginPath(); 
+            cx.moveTo(pos.x, pos.y); 
+            pos = Tool.relativePos(event, cx.canvas); 
+            cx.lineTo(pos.x, pos.y); 
+            cx.stroke(); 
+        }, onEnd); 
     }
 }
 
@@ -144,33 +124,33 @@ class TextTool extends Tool {
 
 class SprayTool extends Tool {
     down (event, cx) {
-    var radius = cx.lineWidth / 2;
-    var area = radius * radius * Math.PI;
-    var dotsPerTick = Math.ceil(area / 30);
+        var radius = cx.lineWidth / 2;
+        var area = radius * radius * Math.PI;
+        var dotsPerTick = Math.ceil(area / 30);
 
-    var currentPos = Tool.relativePos(event, cx.canvas);
-    var spray = setInterval(function() {
-        for (var i = 0; i < dotsPerTick; i++) {
-        var offset = SprayTool.randomPointInRadius(radius);
-        cx.fillRect(currentPos.x + offset.x,
-                currentPos.y + offset.y, 1, 1);
-        }
-    }, 25);
+        var currentPos = Tool.relativePos(event, cx.canvas);
+        var spray = setInterval(function() {
+            for (var i = 0; i < dotsPerTick; i++) {
+            var offset = SprayTool.randomPointInRadius(radius);
+            cx.fillRect(currentPos.x + offset.x,
+                    currentPos.y + offset.y, 1, 1);
+            }
+        }, 25);
 
-    this.trackDrag (function(event) {
-        currentPos = Tool.relativePos(event, cx.canvas);
-    }, function() {
-        clearInterval(spray);
-    });
+        this.trackDrag (function(event) {
+            currentPos = Tool.relativePos(event, cx.canvas);
+        }, function() {
+            clearInterval(spray);
+        });
     }
 
     static randomPointInRadius (radius) {
-    while (true) { 
-        var x = Math.random() * 2 - 1; 
-        var y = Math.random() * 2 - 1; 
-        if (x * x + y * y <= 1) 
-            return {x: x * radius, y: y * radius}; 
-    }
+        while (true) { 
+            var x = Math.random() * 2 - 1; 
+            var y = Math.random() * 2 - 1; 
+            if (x * x + y * y <= 1) 
+                return {x: x * radius, y: y * radius}; 
+        }
     }
 }
 
